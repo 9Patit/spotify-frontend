e452335c225399faf7573931686eaa07a90d4ef3
unknown <patit.sudsat@gmail.com>
2024-04-21 21:46:54 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Login from "./component/dthjr";
+
+
+
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return(
+    <Login />
+
+  )
 }
 
 export default App;
---
+++ src/component/dthjr.jsx
@@ -0,0 +1,17 @@
+const clientId = "77e4fd1425b7460fab2a92271d2bb060";
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}
+    &response_type=code&redirect_uri=http://localhost:5173&
+    scope=streaming%20user-read-email%20playlist-modify-public%20user-read-private
+    %20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+
+const Login = () => {
+  return(
+    <div className="flex items-center justify-center h-screen w-screen">
+      <a className="bg-green-500 text-white px-4 py-2 text-lg rounded-lg" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </div>
+  )
+};
+
+export default Login;
