974d2edeb8dfd9464398cd4418ab1d853978286b
unknown <patit.sudsat@gmail.com>
2024-05-28 21:45:42 +0700

    edit login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,16 +2,15 @@ import Logo from "../img/Logo.png";
 
 const clientId = "e0e2d01268884c5c86bef49f2533af70";
 
-// const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
 
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://spotify-frontend-beta.vercel.app&scope=streaming%20user-read-email%20playlist-modify-public%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://localhost:5173&scope=streaming%20user-read-email%20playlist-modify-public%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 const Login = () => {
   return (
     <div className=" flex  flex-col  justify-center items-center h-screen w-screen bg-[#1DB954] ">
       <div>
-        <img src={Logo} alt="Logo" className="h-[200px]"/>
+        <img src={Logo} alt="Logo" className="h-[200px]" />
       </div>
       <div className=" mt-[100px]">
         <a
@@ -20,6 +19,21 @@ const Login = () => {
         >
           Login with Spotify
         </a>
+      </div>
+      <div>
+        <p>
+          You can use <br />
+          User: <b>test.clonespotify@gmail.com</b>
+          <br />
+          Password: <b>passwordfortest.</b>
+          <br />
+          (login with spotify NOT! google account)to test this project
+          <br />
+          or you need to provide your name and email address for access to the
+          Spotify API
+          <br />
+          by send your Spotify E-mail address to gearzilla13@gmail.com{" "}
+        </p>
       </div>
     </div>
   );
